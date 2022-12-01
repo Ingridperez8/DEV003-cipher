42ed99eff04873f2576a7ad9beec01f460d2dc2e
Ingridperez8 <119013896+Ingridperez8@users.noreply.github.com>
2022-12-01 11:43:47 -0500

    agrega evento al boton cifrar y descrifrar

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+const cipherbtn = document.getElementById("Cifrar");
+const Discipherbtn = document.getElementById("Descifrar");
+
+cipherbtn.addEventListener("click"), function () {
+    const offetValue=parseInt(document.getElementById("offset").value);
+    
+}
